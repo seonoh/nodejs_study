aca38a6a3c626d5260314db0976c1a474d44b042
seonoh <eso0609@naver.com>
2019-05-15 10:28:04 +0900

    viewcount up (sync callback) test

--- wenwoTest/day4/viewcount_up.js
+++ wenwoTest/day4/viewcount_up.js
@@ -1,18 +1,35 @@
-var request = require("request"); 
-let cnt = 0;
-for(let i=0; i<1000; i++) {
-    ++cnt;
-    request("https://linkareer.com/activity/22113")
-    .on('response',function(response){
-        console.log(i+" -->>>  STATUS CODE : " +response.statusCode);
-    })
-    .on('error',function(err){
-        console.log(i+" --->>>   ERROR MSG : " +err);
-    })
+// var request = require("request");
+// let cnt = 0;
+// for (let i = 0; i < 100; i++) {
+//     ++cnt;
+//     request("https://linkareer.com/activity/22113")
+//         .on('response', function (response) {
+//             console.log(i + " -->>>  STATUS CODE : " + response.statusCode);
+//         })
+//         .on('error', function (err) {
+//             console.log(i + " --->>>   ERROR MSG : " + err);
+//         })
+// }
 
+// setTimeout(function(){console.log(1)},1000);
+// setTimeout(function(){console.log(2)},2000);
+
+function loop(){
+    for(var i=1; i<100; i++){
+        delay(i);
+    }
 }
 
-console.log("total cnt : "+cnt);
+function delay(number){
+    setTimeout(function(){console.log(number)},number*1000);
+}
+
+loop();
+
+
+
+
+
 
 
 
