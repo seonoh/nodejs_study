b33b2e0900bcf81ba83d46bc18e1bc9045123fa6
seonoh <eso0609@naver.com>
2019-05-16 17:01:59 +0900

    similar callback promise example..

--- wenwoTest/day5/promiseExample.js
+++ wenwoTest/day5/promiseExample.js
@@ -1,35 +1,54 @@
 let i = 1;
 
-var _promise = function(param){
+var _promise = function (param) {
 
 
-    return new Promise(function(resolve, reject){
 
-        setTimeout(function(){
-            if(param) {
+    return new Promise(function (resolve, reject) {
+
+        setTimeout(function () {
+            if (param) {
 
 
-                if(i<=5){
-                    console.log(`${i} second..`)
+                if (i <= 5) {
+                    console.log(`${i} second..`);
                     i++;
-                    _promise(true);
-                }else{
-                    console.log(`complete !!`);
-                    // resolve(`complete !!`);
+                    _promise(true).then(function(text){
+                        console.log('여기요');
+                    },function(error){
+                        console.log(error);
+                    })
 
+                }else{
+                    resolve("complete!@!@!@");
                 }
-                
+               
+
             } else {
                 reject("failed !!");
             }
 
-        },1000);
+        }, 1000);
     });
 };
 
 _promise(true)
-    .then(function(text){
+    .then(function (text) {
         console.log(text);
-    }, function(error){
+    }, function (error) {
         console.log(error);
     })
+
+// var _promise = Promise.resolve(function(second){
+//     setTimeout(function(){
+
+//         i++;
+//         console.log(`${i} second...`);
+//         _promise(i);
+
+//     },1000)
+// });
+
+// _promise.then(function(value){
+//     console.log(value)
+// })
