3c3874df0c34d96bc9d533bd8891eb0700131e42
seonoh <eso0609@naver.com>
2019-05-17 16:28:17 +0900

    crawling test1

--- wenwoTest/day6/crawlingTest.js
+++ wenwoTest/day6/crawlingTest.js
@@ -1,10 +1,12 @@
 let axiosModule = require('axios');
 let cheerioModule = require('cheerio');
 
-var mangoConfig= {
-    headers:{
-        
-        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+var mangoConfig = {
+    headers: {
+
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+        'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Origin': 'https://www.mangoplate.com'
         // 'User-Agent' : 'PostmanRuntime/7.11.0',
         // 'Accept' : '*/*',
         // 'Cache-Control' : 'no-cache',
@@ -16,7 +18,6 @@ var mangoConfig= {
     }
 }
 
-
 // axios({
 //     url: 'https://www.mangoplate.com/search',
 //     method: 'get',
@@ -26,32 +27,71 @@ var mangoConfig= {
 //   })
 
 const getMangoPlateData = async () => {
-    try{
-         var data = await axiosModule('https://www.mangoplate.com/search/%EB%AA%85%EB%8F%99?keyword=%EB%AA%85%EB%8F%99&page=1',mangoConfig);
-        //  console.log(data['data']);
+    try {
 
-        var $ = cheerioModule.load(data['data'], { decodeEntities: true });
-        
-        // body > main > article > div.column-wrapper > div > div > section > div.search-list-restaurants-inner-wrap > ul > li:nth-child(1) > div:nth-child(1) > figure > figcaption > div > a > h2
-        // for(let i=1; i)
-        
+        var data = await axiosModule('https://www.mangoplate.com/search/%EB%AA%85%EB%8F%99?keyword=%EB%AA%85%EB%8F%99&page=1', mangoConfig);
+
+        var $ = cheerioModule.load(data['data']);
+
         $test = $(`body > main > article > div.column-wrapper > div > div > section > div.search-list-restaurants-inner-wrap > ul > li`),
-        
-        $test.each(function(i,item){
-            $title = $(item).find(`.title`);
-            console.log(i+", "+$title.text());
-            // console.log(i+", "+$title.text()[i][i]);
-        })
 
-        // $test2 = $test.find()
+            $test.each(function (i, item) {
+                $title = $(item).find(`.title`);
+                $href = $(item).find(`figure > figcaption > div > a`).attr('href');
 
-        // console.log("RESULT ====>>>> "+$test);
+                console.log(i + " -> " + $title.text());
 
-    }catch(err){
+                let mangoItem = new MangoItem( $title.text() ,$href );
+                async function test() {
+                    sleep
+                    await getDetailItem(mangoItem.href);
+                } 
+
+                test();
+                
+            
+            })
+
+    } catch (err) {
         console.log(err);
     }
 }
 
+function MangoItem(title, href){
+    this.title = title;
+    this.href = href;
+}
+
+const getDetailItem = async(href) =>{
+    try {
+
+        let result = await axiosModule(`https://www.mangoplate.com${href}`, mangoConfig);
+
+        // console.log(result['data']);
+
+        _ = cheerioModule.load(result['data']);
+
+        _detail = _('body > main > article > div.column-wrapper > div.column-contents > div > section.restaurant-detail > table > tbody');
+
+        _detail.each(function(i,item){
+
+            address = _(item).find('tr.only-desktop>td').text();
+            titlePhoneNum = _(item).find('tr.only-desktop>th').text();
+            phoneNum = _(item).find('tr.only-desktop>td').text();
+            console.log(` 주소 : ${address}`);
+        })
+        
+
+        
+
+
+        
+
+    
+    }catch(err){
+        console.log(err);
+    }
+}
 
 getMangoPlateData();
 
