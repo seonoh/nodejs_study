54860ef1cbbd3c5710114d51e22ce949c373df78
seonoh <eso0609@naver.com>
2019-05-29 11:55:14 +0900

    Jeju Crawaling complete

--- wenwoTest/day12/JejuCrawaling.js
+++ wenwoTest/day12/JejuCrawaling.js
@@ -4,12 +4,14 @@ let cheerioModule = require('cheerio');
 let jejuTotalCnt = ''
 let util = require('./util.js')
 let curSequence = 0;
-let finalData = ''
+
 let jejuResulItemtList = [];
 
 
 const JEJU_MAIN_URL = 'https://api.visitjeju.net/api/contents/list?_siteId=jejuavj&locale=kr&device=pc&cate1cd=cate0000000002&sorting=reviewcnt+desc,+title_kr+asc&region2cd=&pageSize=6&page=1'
-
+const JEJU_FOOD_URL = 'https://api.visitjeju.net/api/contents/list?_siteId=jejuavj&locale=kr&device=pc&cate1cd=cate0000000005&sorting=reviewcnt+desc,+title_kr+asc&region2cd=&pageSize=6&page=1'
+const JEJU_LODGMENT_URL = 'https://api.visitjeju.net/api/contents/list?_siteId=jejuavj&locale=kr&device=pc&cate1cd=cate0000000004&sorting=reviewcnt+desc,+title_kr+asc&region2cd=&pageSize=6&page=1'
+const JEJU_SHOPPING_URL = 'https://api.visitjeju.net/api/contents/list?_siteId=jejuavj&locale=kr&device=pc&cate1cd=cate0000000003&sorting=reviewcnt+desc,+title_kr+asc&region2cd=&pageSize=6&page=1'
 const JEJU_CONFIG = {
 
     'headers': {
@@ -21,16 +23,18 @@ const JEJU_CONFIG = {
 }
 
 //제주 관광정보 페이지 수
-const getJeJuPageCnt = async (jejuFirstUrl) => {
+const getJeJuPageCnt = async (url) => {
     let pageResult = '';
 
     try {
-        pageResult = await axiosModule.get('https://api.visitjeju.net/api/contents/list?_siteId=jejuavj&locale=kr&device=pc&cate1cd=cate0000000002&sorting=reviewcnt+desc,+title_kr+asc&region2cd=&pageSize=6&page=1',
-        JEJU_CONFIG)
+        pageResult = await axiosModule.get(url,JEJU_CONFIG)
 
     } catch (err) {
-        console.log(err)
+        console.log(`getJeJuPageCnt ERROR --->> ${err}`)
+        console.log(`getJeJuPageCnt retry...`)
+        getJeJuPageCnt(url)
     }
+
     jejuTotalCnt = pageResult['data']['totalCount'];
     return jejuTotalCnt;
 
@@ -38,34 +42,50 @@ const getJeJuPageCnt = async (jejuFirstUrl) => {
 }
 
 //제주 관광정보
-const getJeJuData = async (totalCnt) => {
+const getJeJuData = async (totalCnt,curUrl) => {
     let jejuResult = '';
+    let useUrl = curUrl.replace(/pageSize=\d*/,`pageSize=${totalCnt}`)
+
+    console.log(`TOTAL_COUNT : ${totalCnt}`)
 
     try {
-        jejuResult = await axiosModule.get(`https://api.visitjeju.net/api/contents/list?_siteId=jejuavj&locale=kr&device=pc&cate1cd=cate0000000002&sorting=reviewcnt+desc,+title_kr+asc&region2cd=&pageSize=
-        1&page=1`,
+        jejuResult = await axiosModule.get(useUrl,
         JEJU_CONFIG)
 
     } catch (err) {
-        console.log(err)
+        console.log(`getJeJuData ERROR --->> ${err}`)
+        console.log(`getJeJuData retry...`)
+        getJeJuData(totalCnt,curUrl)
+
     }
     let jejuDataList = jejuResult['data']['items']
 
     for(var i=0; i<jejuDataList.length; i++){
-        let imgPath = jejuDataList[i].repPhoto['photoid']['imgpath']
+        let imgPath = ''
+        try{
+            if(jejuDataList[i].repPhoto['photoid'] != null){
+                if(jejuDataList[i].repPhoto['photoid']['imgpath'] != null){
+                    imgPath = jejuDataList[i].repPhoto['photoid']['imgpath']
+                }
+            }
+        }catch(err){
+            imgPath = ''
+        }
+        
+        
         let item = ''
         if(curSequence == 0){
-            item = util.createCrawalingModel('인덱스,이름,주소,태그,전화번호,홈페이지,운영시간,이미지,소개\n')
+            item = util.createCrawalingModel('인덱스','이름','주소','태그','전화번호','홈페이지','운영시간/휴무일','이미지','소개\n')
             
         }else{
             item = util.createCrawalingModel(
-                curSequence,
+                `${curSequence}`,
                 jejuDataList[i].title,
                 jejuDataList[i].address,
                 jejuDataList[i].tag,
                 jejuDataList[i].phoneno,
-                'empty',
-                'empty',
+                ' ',
+                ' ',
                 imgPath,
                 jejuDataList[i].sbst
             ) 
@@ -85,9 +105,17 @@ const getJeJuData = async (totalCnt) => {
 }
 
 const startCrawaling = async() => {
-    await getJeJuData(await getJeJuPageCnt(JEJU_MAIN_URL))
+    start = new Date().getTime(); 
+    await getJeJuData(await getJeJuPageCnt(JEJU_MAIN_URL),JEJU_MAIN_URL)
+    await getJeJuData(await getJeJuPageCnt(JEJU_FOOD_URL),JEJU_FOOD_URL)
+    await getJeJuData(await getJeJuPageCnt(JEJU_LODGMENT_URL),JEJU_LODGMENT_URL)
+    await getJeJuData(await getJeJuPageCnt(JEJU_SHOPPING_URL),JEJU_SHOPPING_URL)
+    util.calcurlateTime(start,'크롤링')
+    start2 = new Date().getTime();
     util.writeData("제주여행정보",jejuResulItemtList)
-    console.log(jejuResulItemtList)
+    util.calcurlateTime(start2,'엑셀 작업')
+    util.calcurlateTime(start,'총 작업')
+
 }
 
 startCrawaling()
