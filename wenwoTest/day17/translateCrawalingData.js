96311e44a15fb5d665de19ee56c474d6a9f54f72
Seonoh Eam <>
2019-06-07 20:44:44 +0900

    chinese translate apply

--- wenwoTest/day17/translateCrawalingData.js
+++ wenwoTest/day17/translateCrawalingData.js
@@ -10,10 +10,10 @@ let customRequestModule = require('../netRequest.js')
 // let jeonjuCrawalingModule = require('../day12/JeonjuCrawaling.js')
 
 
-let kyungbukCrawalingModule = require('../day13/KyungbukCrawaling.js')
+// let kyungbukCrawalingModule = require('../day13/KyungbukCrawaling.js')
 
 // let gangwonData = []
-// let gangwonCrawalingModule = require('../day14/GangwonCrawaling.js')
+let gangwonCrawalingModule = require('../day14/GangwonCrawaling.js')
 
 const loadData = async()=>{
     // let jejuData = await jejuCrawalingModule.startJeJuCrawaling()
@@ -24,11 +24,15 @@ const loadData = async()=>{
     // let translatedJeonjuData = await translateModule.translateLang(jeonjuData)
     // await util.writeData('전주여행정보중국어버전',translatedJeonjuData)
 
-    let kyungbukData = await kyungbukCrawalingModule.startKyungbukCrawaling()
-    let translatedKyungbukData = await translateModule.translateLang(kyungbukData)
-    await util.writeData('경북여행정보중국어버전',translatedKyungbukData)
+    // let kyungbukData = await kyungbukCrawalingModule.startKyungbukCrawaling()
+    // let translatedKyungbukData = await translateModule.translateLang(kyungbukData)
+    // await util.writeData('경북여행정보중국어버전',translatedKyungbukData)
 
-    // gangwonData = await gangwonCrawalingModule.startGangwonCrawaling()
+    let gangwonData = await gangwonCrawalingModule.startGangwonCrawaling()
+    let translatedGangwonData = await translateModule.translateLang(gangwonData)
+    await util.writeData('강원여행정보중국어버전',translatedGangwonData)
+
+
 }
 
 
